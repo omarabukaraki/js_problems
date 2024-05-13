4b09a09ea3e3d7026e195730a276294dda245215
omar abu karaki <abukarakiomar@gmail.com>
2024-05-13 12:05:28 +0300

    solve 2 problem

--- array.js
+++ array.js
@@ -1,1 +1,25 @@
-console.log("omar")
+console.log('Here is : ', 'array');
+
+/*1
+Correct the syntax error
+[ 1,7  9  45, ]
+
+["Str" "alex","moh"
+'the','fox' 'over' lazy, 'dog',  ]
+
+The sol:
+[ 1,7 , 9 , 45 ]
+["Str", "alex","moh" ,'the','fox', 'over' ,"lazy", 'dog', ]
+ */
+
+
+/*
+2
+What the index of "Banana","Tomato"
+var fruits=["Tomato","Banana","Watermelon"]
+
+The sol:
+"Banana" => 1
+"Tomato" => 0
+*/
+
