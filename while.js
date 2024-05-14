c143a6c6c1d42faa84b9ccf7a25049cb970bf59c
omar <abukarakiomar@gmail.com>
2024-05-15 01:14:55 +0300

    p5 commit

--- while.js
+++ while.js
@@ -217,3 +217,108 @@ function repeatChar2(str,char){
     }
     return counter;
 }
+
+
+// 5-
+// function HiSayHelloTo(number){
+//  if(number ===1) return number;
+//  if(number ===2) return `${number-1} and ${number}`;
+//  return `hi ${number} Now Pleas Say Hello To ${HiSayHelloTo(number-1)}`
+// }
+
+
+// 1
+// 1 and 2
+// 1, 2 and 3
+// 1, 2, 3 and 4
+// 1, 2, 3, 4 and 5
+// 1, 2, 3, 4, 5 and 6
+// console.log(HiSayHelloTo(5));
+/*
+5
+Create a function called HiSayHelloTo
+that takes 1 parameter a number that represent
+the nubmer of people come to an event,
+and say hi to each person sepretadly
++ let him/her say hello to each one that comes before him/her
+Ex: welcomeSayHello(3);
+=>
+hi 1
+hi 2 Now Pleas Say Hello To 1
+hi 3 Now Pleas Say Hello To 1 and 2
+Ex: welcomeSayHello(7); =>
+hi 1
+hi 2 Now Pleas Say Hello To 1
+hi 3 Now Pleas Say Hello To 1 and 2
+hi 4 Now Pleas Say Hello To 1, 2 and 3
+hi 5 Now Pleas Say Hello To 1, 2, 3 and 4
+hi 6 Now Pleas Say Hello To 1, 2, 3, 4 and 5
+hi 7 Now Pleas Say Hello To 1, 2, 3, 4, 5 and 6
+*/
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+console.log(`x : ${x}`);
+
+var x = "omar";
