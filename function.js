01334a422c7640b35be20788ff11eb82bfdaa3c2
omar abu karaki <abukarakiomar@gmail.com>
2024-05-15 13:12:35 +0300

    solve problem 18

--- function.js
+++ function.js
@@ -191,19 +191,11 @@ function positive(number){
 
 
 
+// 18-
+function fullName(fName,lName){
+    return `"${fName} ${lName}"`
+}
 
-/*
-18
-Write a function called fullName
-that accept two parameters, firstName and lastName,
-and returns the firstName and lastName concatenated
-together with a space in between.
-
-Ex: fullName("Adam","McCallen")
-=> "Adam McCallen"
-Ex: fullName("Alex", "Mercer")
-=> "Alex Mercer"
-*/
 
 
 /*
